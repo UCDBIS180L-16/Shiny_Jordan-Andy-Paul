23afdda80c0c9f5098f7c8cb3778552c25512d93
Andy Pham <apham@ucdavis.edu>
2016-05-10 13:44:25 -0700

    Added Checkbox as option for regression lines

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -11,7 +11,7 @@ library(shiny)
 library(ggplot2)
 library(reshape2)
 
-#RiceData<-read.csv("/home/handy/Bioinformatics_Lab_BIS180L/Shiny_Jordan-Andy-Paul/RiceSNPData/RiceDiversity.44K.MSU6.Phenotypes.csv",header=T)
+RiceData<-read.csv("/home/handy/Bioinformatics_Lab_BIS180L/Shiny_Jordan-Andy-Paul/RiceSNPData/RiceDiversity.44K.MSU6.Phenotypes.csv",header=T)
 #RiceData<-read.csv("~/MEGAsync/Assignment_4_Zaman.Paul/RiceSNPData/RiceDiversity.44K.MSU6.Phenotypes.csv",header=T)
 
 Rice.melt<-melt(RiceData,id.vars="Region")
@@ -31,16 +31,32 @@ shinyServer(function(input, output) {
   Rice.melt.ss<-subset(RiceData,variable=input$trait)
   output$boxPlot <- renderPlot({
     
-    # set up the plot
-    pl <- ggplot(data = Rice.melt.ss,
-                 #Use aes_string below so that input$trait is interpreted
-                 #correctly.  The other variables need to be quoted
-                 aes_string(x=input$xtrait,
-                            y=input$ytrait,
-                            color=input$color                 )
-    )
-    # draw the boxplot for the specified trait
-    pl + geom_point(size=input$decimal) + geom_smooth(method=lm,se=FALSE)
+    if(input$regression == TRUE){
+      # set up the plot
+      pl <- ggplot(data = Rice.melt.ss,
+                  #Use aes_string below so that input$trait is interpreted
+                  #correctly.  The other variables need to be quoted
+                  aes_string(x=input$xtrait,
+                              y=input$ytrait,
+                              color=input$color                 )
+      )
+      # draw the boxplot for the specified trait
+      pl + geom_point(size=input$decimal) + geom_smooth(method=lm,se=FALSE)
+    }
+    else{
+      # set up the plot
+      pl <- ggplot(data = Rice.melt.ss,
+                   #Use aes_string below so that input$trait is interpreted
+                   #correctly.  The other variables need to be quoted
+                   aes_string(x=input$xtrait,
+                              y=input$ytrait,
+                              color=input$color                 )
+      )
+      # draw the boxplot for the specified trait
+      pl + geom_point(size=input$decimal)
+      
+    }
   })
+  
 })
 
